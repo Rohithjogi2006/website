28788b9a4889151a92e513d1e92bbbdbf0cfb4f6
Rohithjogi2006 <rohithjogi51@gmail.com>
2025-07-04 17:03:50 +0530

    alert using JS done

---
+++ client/login page/login.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === "" || password === "") {
+    e.preventDefault(); // stop form submission
+    alert("Please enter both username and password.");
+  }
+});
